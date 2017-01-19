2dccfa7aaa53c2917b3607a328034ffa5871ebcd
qiuzhenguang <qiuzhenguang@baidu.com>
2017-01-19 10:58:46 +0800

    modify

--- nginx-mod_h264/build.sh
+++ nginx-mod_h264/build.sh
@@ -46,3 +46,35 @@ curl "http://localhost:8501/a.mp4?ts=5&te=20" -svo /dev/null
 #Stop Nginx:
 ./objs/nginx -s stop -c conf/nginx.conf -p .
 
+
+
+#add some logs to nginx_mod_h264_streaming
+
+#1. nginx_mod_h264_streaming-2.3.2/src/ngx_http_streaming_module.c   Line:264
+  #if(r->args.len) {
+  #    ngx_log_debug(NGX_LOG_DEBUG_CORE, log, 0, "Go here, r->args=%V, %s, %d", &(r->args),  __FILE__, __LINE__);
+  #} else {
+  #    ngx_log_debug(NGX_LOG_DEBUG_CORE, log, 0, "Go here, r->args.len=0, %s, %d", __FILE__, __LINE__);
+  #}
+
+
+#2. nginx_mod_h264_streaming-2.3.2/src/moov.c   Line:143, 173
+        #mp4_log_trace("ts=%.2f (seconds)\n",
+        #         trak->samples_[sample_index].pts_ / (float)trak_time_scale);
+
+
+#moov.c Line:184   mp4_log_trace("final start=%"PRId64"\n", start);
+#moov.c Line:185   mp4_log_trace("final end=%"PRId64"\n", end);
+
+#3.mp4_io.c
+#Line:71  freopen("/home/work/qiuzhenguang/mp4/nginx/nginx-1.8.1/logs/module.log", "a", stdout);
+#Line:77  fflush(stdout);
+
+
+
+
+
+
+
+
+
