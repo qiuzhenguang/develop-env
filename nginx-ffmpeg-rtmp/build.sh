3a09de51abe728c92a18864dd51ed7cb1170bb0b
qiuzhenguang <qiuzhenguang@163.com>
2017-01-31 23:38:07 +0800

    modify nginx-rtmp build.sh

--- nginx-ffmpeg-rtmp/build.sh
+++ nginx-ffmpeg-rtmp/build.sh
@@ -1,1 +1,14 @@
-# download nginx
+#build nginx-1.8.1 and rtmp and pcre  and ffmpeg
+
+#download nginx-1.8.1
+
+wget http://nginx.org/download/nginx-1.8.1.tar.gz 
+
+tar -xzvf nginx-1.8.1.tar.gz 
+
+#download ngx-rtmp-module
+
+#download pcre
+wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz
+tar -xzvf pcre-8.40.tar.gz
+
