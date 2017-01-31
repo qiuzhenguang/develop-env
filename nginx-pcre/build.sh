bef4505d920584e2c3059356742bdcaa74083a5a
qiuzhenguang <qiuzhenguang@163.com>
2017-01-31 23:28:48 +0800

    add pcre build.sh

--- nginx-pcre/build.sh
+++ nginx-pcre/build.sh
@@ -1,2 +1,46 @@
-#download nginx
+#build nginx-1.8.1 and pcre
+
+#download nginx-1.8.1
+
+wget http://nginx.org/download/nginx-1.8.1.tar.gz 
+
+tar -xzvf nginx-1.8.1.tar.gz 
+
 #download pcre
+wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz
+tar -xzvf pcre-8.40.tar.gz
+
+#configure
+
+cd nginx-1.8.1 
+
+PWD=`pwd`
+
+./configure  \
+--with-pcre=$PWD/../pcre-8.40/
+
+make
+
+
+#conf:
+sed -i "s/listen       80;/listen       8501;/g" ./conf/nginx.conf
+mkdir logs
+touch logs/error.log
+
+#run:
+#Test nginx.conf:
+./objs/nginx -t -c conf/nginx.conf -p .
+
+#Run Nginx:
+./objs/nginx -c conf/nginx.conf -p .
+ps -ef | grep `cat logs/nginx.pid`
+
+#Test mytest module
+curl http://localhost:8501/index.html
+
+
+#Stop Nginx:
+./objs/nginx -s stop -c conf/nginx.conf -p .
+
+
+
