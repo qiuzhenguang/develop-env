5955b06559a243551fc598d12a1f38c4f5e0e042
work <work@qqvideo-test-server.(none)>
2017-03-14 16:49:09 +0800

    modify nginx-lua and nginx-openssl.

--- nginx-lua/build.sh
+++ nginx-lua/build.sh
@@ -1,1 +1,50 @@
-# download nginx
+#https://github.com/openresty/lua-nginx-module#installation
+
+#OpenResty bundle
+
+#Download openresty
+#openresty-1.11.2.2.tar.gz   4.1MB   PGP   Changes - 17 November 2016
+
+wget https://openresty.org/download/openresty-1.11.2.2.tar.gz --no-check-certificate
+tar -xzvf openresty-1.11.2.2.tar.gz
+
+#download pcre
+wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz --no-check-certificate
+tar -xzvf pcre-8.40.tar.gz
+
+#download openssl
+#wget https://www.openssl.org/source/openssl-1.1.0e.tar.gz  --no-check-certificate
+#tar -xzvf openssl-1.1.0e.tar.gz
+
+cd openresty-1.11.2.2
+
+PWD=`pwd`
+
+sed -i "s/disable-shared/disable-shared enable_cpp=no/g"  ./bundle/nginx-1.11.2/auto/lib/pcre/make
+
+./configure --with-luajit \
+ --with-pcre=$PWD/../pcre-8.40/  \
+ --without-http_ssl_module \
+ --prefix=$PWD/nginx
+
+# --with-openssl=$PWD/../openssl-1.1.0e/   \
+
+make 
+make install
+
+cd nginx/nginx
+
+#conf:
+sed -i "s/listen       80;/listen       8501;/g" ./conf/nginx.conf
+sed -i "s/root   html;/content_by_lua 'ngx.say(\"Hello Lua\")';/g" ./conf/nginx.conf
+
+
+sbin/nginx  -c conf/nginx.conf 
+ps -ef | grep `cat logs/nginx.pid`
+
+curl http://127.0.0.1:8501/
+
+sbin/nginx  -c conf/nginx.conf  -s stop
+ps -ef | grep `cat logs/nginx.pid`
+
+
--- nginx-openssl/build.sh
+++ nginx-openssl/build.sh
@@ -1,1 +1,48 @@
-#to add
+#build nginx-1.8.1 and openssl
+
+#download nginx-1.8.1
+
+wget http://nginx.org/download/nginx-1.8.1.tar.gz 
+
+tar -xzvf nginx-1.8.1.tar.gz 
+
+#download openssl
+wget https://www.openssl.org/source/openssl-1.1.0e.tar.gz  --no-check-certificate
+tar -xzvf penssl-1.1.0e.tar.gz
+
+#configure
+
+cd nginx-1.8.1 
+
+PWD=`pwd`
+
+./configure  \
+--with-openssl=$PWD/../penssl-1.1.0e/   \
+--without-http_rewrite_module
+
+make
+
+
+#conf:
+sed -i "s/listen       80;/listen       8501;/g" ./conf/nginx.conf
+mkdir logs
+touch logs/error.log
+
+#run:
+#Test nginx.conf:
+./objs/nginx -t -c conf/nginx.conf -p .
+
+#Run Nginx:
+./objs/nginx -c conf/nginx.conf -p .
+ps -ef | grep `cat logs/nginx.pid`
+
+#Test mytest module
+curl http://localhost:8501/index.html
+
+
+#Stop Nginx:
+./objs/nginx -s stop -c conf/nginx.conf -p .
+
+
+# This file is not test ok.
+
