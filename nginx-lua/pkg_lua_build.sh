6e614da6f8fbe57e875439745102c44d708bf384
qiuzhenguang <qiuzhenguang@163.com>
2017-03-19 17:10:51 +0800

    add lua5.1.5 build

--- nginx-lua/pkg_lua_build.sh
+++ nginx-lua/pkg_lua_build.sh
@@ -1,6 +1,6 @@
 #https://github.com/openresty/lua-nginx-module#installation
 
-#Download nginx pcre LuaJIT nginx_devel_kit ngx_lua
+#Download nginx pcre Lua5.1 nginx_devel_kit ngx_lua
 
 #download nginx-1.8.1
 #https://github.com/nginx/nginx/commits/master/src/core/ngx_cycle.h  
@@ -10,36 +10,36 @@ tar -xzvf nginx-1.11.10.tar.gz
 
 #download pcre
 #must add pcre, because ngx_devel_kit need http_rewrite_module/ngx_http_rewrite_module
-wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz --no-check-certificate
+wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz -O pcre-8.40.tar.gz --no-check-certificate
 tar -xzvf pcre-8.40.tar.gz
 
-#download LuaJIT
-wget http://luajit.org/download/LuaJIT-2.1.0-beta2.tar.gz
-tar -xzvf LuaJIT-2.1.0-beta2.tar.gz
+#download Lua5.1
+wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
+tar -xzvf lua-5.1.5.tar.gz
 
 #download ngx_devel_kit
-wget https://github.com/simpl/ngx_devel_kit/archive/v0.3.0.tar.gz --no-check-certificate
+wget https://github.com/simpl/ngx_devel_kit/archive/v0.3.0.tar.gz -O v0.3.0.tar.gz --no-check-certificate
 tar -xzvf v0.3.0.tar.gz
 
-wget https://github.com/openresty/lua-nginx-module/archive/v0.10.7.tar.gz  --no-check-certificate
+wget https://github.com/openresty/lua-nginx-module/archive/v0.10.7.tar.gz  -O v0.10.7.tar.gz --no-check-certificate
 tar -xzvf v0.10.7.tar.gz
 
-cd ./LuaJIT-2.1.0-beta2
+cd ./lua-5.1.5
 PWD=`pwd`
-make DPREFIX=$PWD
-make install DPREFIX=$PWD
+make linux
+make local
 cd -
 
 cd nginx-1.11.10
 PWD=`pwd`
 sed -i "s/disable-shared/disable-shared enable_cpp=no/g"  ./auto/lib/pcre/make
 
-export LUAJIT_LIB=$PWD/../LuaJIT-2.1.0-beta2/lib
-export LUAJIT_INC=$PWD/../LuaJIT-2.1.0-beta2/include/luajit-2.1
+export LUA_LIB=$PWD/../lua-5.1.5/lib
+export LUA_INC=$PWD/../lua-5.1.5/include
 
 #configure
 ./configure \
-         --with-ld-opt="-Wl,-rpath,$PWD/../LuaJIT-2.1.0-beta2/lib" \
+         --with-ld-opt="-Wl,-rpath,$PWD/../lua-5.1.5/lib" \
          --add-module=$PWD/../ngx_devel_kit-0.3.0 \
          --add-module=$PWD/../lua-nginx-module-0.10.7  \
          --with-pcre=$PWD/../pcre-8.40/ 
@@ -49,7 +49,7 @@ make
 #conf:
 sed -i "s/listen       80;/listen       8501;/g" ./conf/nginx.conf
 sed -i "s/#error_log/error_log/g" ./conf/nginx.conf
-sed -i "s/root   html;/content_by_lua 'ngx.say(\"Hello LuaJIT\")';/g" ./conf/nginx.conf
+sed -i "s/root   html;/content_by_lua 'ngx.say(\"Hello Lua5.1.5\")';/g" ./conf/nginx.conf
 
 mkdir logs
 touch logs/error.log
