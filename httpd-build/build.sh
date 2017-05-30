39a8dcbe0c6888c70648f6758aec81411e73def3
qiuzhenguang <qiuzhenguang@163.com>
2017-05-30 14:26:16 +0800

    add httpd build.sh

--- httpd-build/build.sh
+++ httpd-build/build.sh
@@ -1,2 +1,54 @@
 #build httpd
 
+wget https://archive.apache.org/dist/httpd/httpd-2.2.25.tar.gz  --no-check-certificate
+
+tar -xzvf httpd-2.2.25.tar.gz
+
+cd httpd-2.2.25
+
+./configure
+
+make
+
+mkdir conf
+
+mkdir logs
+
+mkdir htdocs
+
+echo aaaaaaaa > htdocs/a.txt
+
+
+cd conf/
+
+cp ../docs/conf/httpd.conf httpd.conf.mod
+cp httpd.conf.mod httpd.conf
+
+cat httpd.conf.mod | sed "s/\@\@ServerRoot\@\@/\.\//g" > httpd.conf
+cp httpd.conf httpd.conf.mod
+
+cat httpd.conf.mod | sed "s/\@\@Port\@\@/8099/g" > httpd.conf
+cp httpd.conf httpd.conf.mod
+
+cat httpd.conf.mod | sed "s/\@\@LoadModule\@\@//g" > httpd.conf
+cp httpd.conf httpd.conf.mod
+
+cat httpd.conf.mod | sed "s/\/usr\/local\/apache2\//\.\//g" > httpd.conf
+cp httpd.conf httpd.conf.mod
+
+cat httpd.conf.mod | sed "s/Deny from all/Allow from all/g" > httpd.conf
+rm httpd.conf.mod
+
+cd ../
+
+
+cp ./docs/conf/mime.types ./conf/
+
+./httpd -d ./
+curl http://localhost:8099/a.txt
+
+ps -ef | grep httpd
+killall lt-httpd
+sleep 1
+ps -ef | grep httpd
+
