f58281f98d707fe2e2ad06d475e7d4f6ea8e936e
qiuzhenguang <qiuzhenguang@bytedance.com>
2019-02-12 11:46:26 +0800

    add app_test.cc

--- leveldb-build/build.sh
+++ leveldb-build/build.sh
@@ -4,3 +4,19 @@
  cd build/
  cmake ..
  make
+
+cd leveldb
+mkdir app
+cd app/
+vim app_test.cc
+
+vim ../CMakeLists.txt 
+#if(NOT BUILD_SHARED_LIBS)
+#+    leveldb_test("${PROJECT_SOURCE_DIR}/app/app_test.cc")cd ../build/
+
+cmake  -DCMAKE_BUILD_TYPE=Debug ..
+make
+./app_test 
+gdb ./app_test
+
+#https://zhuanlan.zhihu.com/p/34657032
